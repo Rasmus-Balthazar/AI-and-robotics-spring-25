2bb3813869e0dcbc4c074b53113e22caec1c2cd7
Rasmus Balthazar Rødgaard Andreasen <rban@itu.dk>
2025-03-23 20:49:29 +0100

    Add atomic operations for interpreting moves

--- Mandatory 2/sakoban.py
+++ Mandatory 2/sakoban.py
@@ -4,6 +4,7 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Axis
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 from urandom import randint
+from planner_final import solve_puzzle
 
 hub = PrimeHub()
 
@@ -26,10 +27,6 @@ def turn(angle):
 def move(distance: int):
     drive_base.straight(distance, Stop.COAST_SMART, wait=True)
 
-def calibrate():
-    initial_heading = hub.imu.heading()
-    return initial_heading
-# method for keeping direction.
 
 desk_color = Color(h=200, s=15, v=21)
 
@@ -61,7 +58,9 @@ def calibrate_movement(speed, db: DriveBase, lcs: ColorSensor, rcs: ColorSensor)
 while not hub.imu.ready():
     print('calibrating')
     wait(1000)
-
+#We assume that we start north, so this will be the baseline for north.
+initial_heading = hub.imu.heading()
+current_heading = initial_heading 
 
 def noise(n, vol: Int = 30):
     hub.speaker.volume(vol)
@@ -74,9 +73,77 @@ reached_goal = False
 debug_mode = True
 hub.speaker.volume(10)
 
+#start atomic operations ------------------------------
 junction_detected = lambda s : s.reflection() < 6
 
+EAST =  ( 0, 1)
+EAST_ANGLE = 90
+WEST =  ( 0,-1)
+WEST_ANGLE = 180
+SOUTH = ( 1, 0)
+SOUTH_ANGLE = 270
+NORTH = (-1, 0)
+NORTH_ANGLE = 0 #maybe make this 360 degrees
 
+def turn_east():
+    current_heading = hub.imu.heading()
+    distance_to_turn = EAST_ANGLE - current_heading 
+    turn(distance_to_turn)
+def turn_west():
+    current_heading = hub.imu.heading()
+    distance_to_turn = WEST_ANGLE - current_heading 
+    turn(distance_to_turn)
+def turn_north():
+    current_heading = hub.imu.heading()
+    distance_to_turn = NORTH_ANGLE - current_heading 
+    turn(distance_to_turn)
+def turn_south():
+    current_heading = hub.imu.heading()
+    distance_to_turn = SOUTH_ANGLE - current_heading 
+    turn(distance_to_turn)
+
+def turn_to_direction(direction):
+    current_heading = hub.imu.heading()
+    turn(current_heading - direction)
+    current_heading = hub.imu.heading()
+
+def double_junction_check():
+    return junction_detected(sensor_left) or junction_detected(sensor_right)
+
+def move_to_next_junction():
+    drive_base.straight(16, Stop.COAST_SMART, wait=False)
+    while hub.imu.acceleration() != 0:
+        #we are still moving, try and detect junction
+        if double_junction_check():
+            break #we must have reached a junction
+    if not double_junction_check():
+        while not double_junction_check():
+            calibrate_movement(100, drive_base, sensor_left, sensor_right)
+    #update state
+
+def determine_move(move, position):
+    #Consider that we have to move twice if we want to mimic sakoban
+    if move == EAST:
+        turn_to_direction(EAST_ANGLE)
+        #move to next junction to the east
+    elif move == WEST:
+        turn_to_direction(WEST_ANGLE)
+        #Move to next junction to the west
+    elif move == SOUTH:
+        turn_to_direction(SOUTH_ANGLE)
+        #Move to next junction to the south
+    elif move == NORTH:
+        turn_to_direction(NORTH_ANGLE)
+        #Move to next junction to the north
+    else:
+        return 0
+       # Could not determine a move
+    move_to_next_junction()
+    return 1
+
+
+
+#end atomic operations ------------------------------
 while not reached_goal:
     if(junction_detected(sensor_left)):
         #
